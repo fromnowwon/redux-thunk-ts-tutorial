23de89fb86a098b97af5d78765758625351a0a63
cw <fromnowwon@gmail.com>
2022-07-19 23:43:41 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { RootStore } from "./store/index";
+import { useState } from "react";
+import { GetPokemon } from "./store/actions/PokemonActions";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const dispatch = useDispatch();
+	const [pokemonName, setPokemonName] = useState("");
+	const pokemonState = useSelector((state: RootStore) => state.pokemon);
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setPokemonName(event.target.value);
+	};
+	const handleSubmit = () => dispatch(GetPokemon(pokemonName) as any);
+
+	console.log(pokemonState);
+
+	return (
+		<div className="App">
+			<input type="text" onChange={handleChange} />
+			<button onClick={handleSubmit}>Search</button>
+			<div>
+				{pokemonState.pokemon && (
+					<div>
+						<h1>{pokemonState.pokemon.name}</h1>
+						<img src={pokemonState.pokemon.sprites.front_default} alt="" />
+						<h2>Abilities</h2>
+						{pokemonState.pokemon.abilities.map(ability => {
+							return <p>{ability.ability.name}</p>
+						})}
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default App;
